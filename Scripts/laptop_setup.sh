0b34c0850888b0e20182f73623d09779271035cd
Thwarē Felix <67859169+OldLorekeeper@users.noreply.github.com>
2025-11-13 19:18:23 +0000

    Update laptop_setup.sh

--- Scripts/laptop_setup.sh
+++ Scripts/laptop_setup.sh
@@ -16,4 +16,13 @@ yay -S --needed --noconfirm - < "$SCRIPT_DIR/laptop_pkg.txt"
 echo "--- Enabling laptop services ---"
 sudo systemctl enable --now power-profiles-daemon.service
 
+# Apply Laptop Kernel Parameters
+echo "--- Applying laptop-specific kernel parameters ---"
+sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ amdgpu.ppfeaturemask=0xffffffff hugepages=512 video=2560x1600@60 amd_pstate=active"/' /etc/default/grub
+
+echo "--- Rebuilding GRUB configuration ---"
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# End
 echo "--- Laptop-Specific Setup Complete ---"
+echo "Please complete any remaining manual steps, then REBOOT."
