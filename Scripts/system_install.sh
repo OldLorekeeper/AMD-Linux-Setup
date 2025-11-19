bcc063b9dc2572cb01df2df296ebdf3e12c826ac
Thwarē Felix <67859169+OldLorekeeper@users.noreply.github.com>
2025-11-19 09:05:29 +0000

    Update system_install.sh

--- Scripts/system_install.sh
+++ Scripts/system_install.sh
@@ -8,21 +8,25 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
-echo "--- Setting keyboard layout to UK ---"
+# Colour Codes
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}--- Setting keyboard layout to UK ---${NC}"
 loadkeys uk
 
-echo "--- Enabling parallel downloads in pacman.conf ---"
+echo -e "${GREEN}--- Enabling parallel downloads in pacman.conf ---${NC}"
 # Note: sudo is not needed as we are root on the ISO
 sed -i 's/^#*\(ParallelDownloads = \).*/\1100/' /etc/pacman.conf
 
-echo "--- Updating mirrorlist (UK/EU) ---"
+echo -e "${GREEN}--- Updating mirrorlist (UK/EU) ---${NC}"
 reflector --country GB,IE,NL,DE,FR,EU --age 12 --protocol https --sort rate --fastest 10 --save /etc/pacman.d/mirrorlist
 
-echo "--- Downloading archinstall configuration... ---"
+echo -e "${GREEN}--- Downloading archinstall configuration... ---${NC}"
 curl -sL -o /root/archinstall_config.json "https://raw.githubusercontent.com/OldLorekeeper/AMD-Linux-Setup/main/Scripts/archinstall_config.json"
 
-echo "--- Syncing pacman and updating archinstall ---"
+echo -e "${GREEN}--- Syncing pacman and updating archinstall ---${NC}"
 pacman -Sy archinstall
 
-echo "--- Preparation complete. Launching archinstall... ---"
+echo -e "${GREEN}--- Preparation complete. Launching archinstall... ---${NC}"
 archinstall --config /root/archinstall_config.json
