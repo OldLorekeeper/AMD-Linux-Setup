0ea527071dfbe9339d67c48ec0512c82e51872bc
Thwarē Felix <67859169+OldLorekeeper@users.noreply.github.com>
2025-11-21 06:13:15 +0000

    Update laptop_setup.sh

--- Scripts/laptop_setup.sh
+++ Scripts/laptop_setup.sh
@@ -13,23 +13,34 @@ NC='\033[0m' # No Color
 
 echo -e "${GREEN}--- Starting Laptop-Specific Setup ---${NC}"
 
-# Install laptop packages from list
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+# 1. Install laptop packages from list
 echo -e "${GREEN}--- Installing laptop packages from laptop_pkg.txt ---${NC}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 yay -S --needed --noconfirm - < "$SCRIPT_DIR/laptop_pkg.txt"
 
-# Enable laptop services
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+# 2. Enable laptop services
 echo -e "${GREEN}--- Enabling laptop services ---${NC}"
 sudo systemctl enable --now power-profiles-daemon.service
 
-# Apply Laptop Kernel Parameters
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+# 3. Apply Laptop Kernel Parameters
 echo -e "${GREEN}--- Applying laptop-specific kernel parameters ---${NC}"
 # This command captures the existing parameters and appends the new ones before the final quote
 sudo sed -i 's/^\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$/\1 amdgpu.ppfeaturemask=0xffffffff hugepages=512 video=2560x1600@60 amd_pstate=active"/' /etc/default/grub
-
 echo -e "${GREEN}--- Rebuilding GRUB configuration ---${NC}"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# End
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+# 4. End
 echo -e "${YELLOW}--- Laptop-Specific Setup Complete ---${NC}"
 echo "Please complete any remaining manual steps, then REBOOT."
