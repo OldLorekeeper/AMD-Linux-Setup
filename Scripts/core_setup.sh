11411c60b1bc4b17d517d666468b5b1205161b8a
OldLorekeeper <cmptait@gmail.com>
2025-12-01 10:22:41 +0000

    vault backup: 2025-12-01 10:22:41

--- Scripts/core_setup.sh
+++ Scripts/core_setup.sh
@@ -96,7 +96,8 @@ echo -e "${GREEN}--- Configuring CachyOS Zen 4 Repositories ---${NC}"
 sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key F3B607488DB35A47
 # Download Mirrorlists directly
-sudo pacman -U 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+sudo pacman -U --noconfirm \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst'
 # Update pacman.conf
